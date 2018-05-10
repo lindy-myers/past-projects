9634915a7d435e73f77ae9f3f0cf8fc32934a529
lmyers24 <lmyers@uoregon.edu>
2018-05-10 11:46:36 -0700

    add files

---
+++ Saferide/server.js
@@ -0,0 +1,124 @@
+//This is the Data Base
+
+var mongodb = require('mongodb');
+var mongoose= require('mongoose');
+mongoose.connect('mongodb://localhost/RiderDB')
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to DB');  
+});
+var riderSchema = mongoose.Schema({
+    name: String,
+    uoid: Number,
+    phone: Number,
+    partySize: Number,
+    time: Number,
+    currentLocation: String,
+    dropLocation: String,
+    additionalInfo: String,
+}); 
+var Rider= mongoose.model('Rider', riderSchema);
+
+
+
+
+
+
+
+//This is the Server
+
+
+var express        =         require("express");
+var bodyParser     =         require("body-parser");
+var app            =         express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname + "/saferide.html");
+  console.log('client connected');
+});
+app.get('/style.css',function(req,res){
+  res.sendFile(__dirname + "/style.css");
+  //console.log('Style Sheet Sent to Client');
+});
+app.get('/saferide_banner.png',function(req,res){
+    res.sendFile(__dirname + "/saferide_banner.png");
+    //console.log("Banner pic Sent to Client");
+});
+
+
+
+app.post('/submitInfoFromClient',function(req,res){
+  var nameFromClient=req.body.name; 
+  var uoidFromClient=req.body.uoid;
+  var phoneFromClient=req.body.phone;
+  var partySizeFromClient=req.body.partySize;
+  var timeFromClient=req.body.time;
+  var currentLocationFromClient=req.body.currentLocation;
+  var dropLocationFromClient=req.body.dropLocation;
+  var additionalInfoFromClient=req.body.additionalInfo;
+  //console.log(user_name);
+  //console.log(user_password);
+  var newRider= new Rider({ 
+    name: nameFromClient,
+    uoid: uoidFromClient,
+    phone: phoneFromClient,
+    partySize: partySizeFromClient,
+    currentLocation: currentLocationFromClient,
+    time: timeFromClient,
+    dropLocation: dropLocationFromClient,
+    additionalInfo: additionalInfoFromClient})
+
+  console.log("created RIDER " + newRider.name);
+
+  newRider.save(function(err,data)
+  {
+      if(err)console.log('error on save');
+      else console.log('Saved Data: ', data);
+  });
+
+  res.send('sucess');
+});
+
+app.post('/dispatchQuery',function(req,toClient){
+  Rider.findOne(function(err,res){
+    if (err)return console.error(err);
+    console.log(res);
+    toClient.send(res);
+    // toClient.send("Information you wanted");
+  });
+});
+
+app.listen(3001,function(){
+  console.log("Started on PORT 3000");
+})
+
+
+
+
+
+// var JaneDoe= new Rider({ name: 'jane'});
+
+// JaneDoe.save(function(err,data){
+//     if(err)console.log('error on save');
+//     else console.log('Saved Data: ', data);
+//   });
+
+function test(){
+
+  console.log('REQUEST START_________');
+  Rider.find(function(err,res){
+    if (err)return console.error(err);
+    console.log(res);
+  });
+};
+
+
+
+ // setTimeout(test,1000);
+
+
